947b489125e5dd3d790d6f46fbc73f31ef06e453
Miranda M <miranda.esmer.50@gmail.com>
2024-02-27 09:58:33 -0600

    solved problem 28

--- projectEuler21to30.py
+++ projectEuler21to30.py
@@ -68,6 +68,29 @@ class ProjectEuler21to30:
 				return idx + 1  # mem is 0-indexed
 		return 0
 	
+	def problem28(self) -> int:
+		# Starting with the number 1 and moving to the right in a clockwise
+		# direction a 5 by 5 spiral is formed as follows:
+		#     21 22 23 24 25
+		#     20  7  8  9 10
+		#     19  6  1  2 11
+		#     18  5  4  3 12
+		#     17 16 15 14 13
+		# It can be verified that the sum of the numbers on the diagonals is
+		# 101. What is the sum of the numbers on the diagonals in a 1001 by
+		# 1001 spiral formed in the same way?
+		i = 1
+		gap = 1
+		limit = 1001 * 1001
+		diagonals = [1]
+		
+		while i < limit:
+			for _ in range(0, 4):
+				i += gap + 1
+				diagonals.append(i)
+			gap += 2
+		return sum(diagonals)
+	
 	def problem29(self) -> int:
 		# Consider all integer combinations of a^b for 2<=a<=5 and 2<=b<=5,
 		# if they are then placed in numerical order, with any repeats removed,
@@ -105,9 +128,9 @@ if __name__ == "__main__":
 	# print(euler.problem21())  # solved
 	# print(euler.problem23())  # TODO unsolved
 	# print(euler.problem24())  # TODO unsolved
-	print(euler.problem25())  # solved
+	# print(euler.problem25())  # solved
 	# print(euler.problem26())  # TODO unsolved
 	# print(euler.problem27())  # TODO unsolved
-	# print(euler.problem28())  # TODO unsolved
+	# print(euler.problem28())  # solved
 	# print(euler.problem29())  # solved
 	# print(euler.problem30())  # solved
